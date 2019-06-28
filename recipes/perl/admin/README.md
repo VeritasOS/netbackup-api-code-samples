e0013a8d563ee03560ce32d3a4026f54734bbcff
Swarada Navale <swarada.navale@veritas.com>
2019-06-28 14:32:42 -0500

    NetBackup 8.2 API Perl scripts

---
+++ recipes/perl/admin/README.md
@@ -0,0 +1,18 @@
+### NetBackup API Code Samples for perl
+
+This directory contains code samples to invoke NetBackup admininstration REST APIs using perl.
+
+#### Disclaimer
+
+These scripts are only meant to be used as a reference. If you intend to use them in production, use it at your own risk.
+
+#### Pre-requisites:
+
+- NetBackup 8.2 or higher
+- Perl 5.20.2 or higher
+
+#### Executing the recipes in perl
+
+Use the following commands to run the perl samples.
+- `perl get_processes.pl -nbmaster <masterServer> -username <username> -password <password> [-domainName <domainName>] [-domainType <domainType>] -client <client>`
+- `perl get_services.pl -nbmaster <masterServer> -username <username> -password <password> [-domainName <domainName>] [-domainType <domainType>] -client <client>`
---
+++ recipes/perl/admin/get_services.pl
@@ -0,0 +1,189 @@
+#!/usr/bin/env perl
+
+use LWP::UserAgent;
+use LWP::Protocol::https;
+print "LWP::UserAgent: ".LWP::UserAgent->VERSION,"\n";
+print "LWP::Protocol::https: ".LWP::Protocol::https->VERSION,"\n";
+use JSON;
+use Getopt::Long qw(GetOptions);
+
+require 'api_requests.pl';
+
+#
+# The token is the key to the NetBackup AuthN/AuthZ scheme.  You must login and get a token
+# and use this token in your Authorization header for all subsequent requests.  Token validity
+# is fixed at 24 hours
+#
+my $token;
+
+my $content_type_v3 = "application/vnd.netbackup+json; version=3.0";
+my $protocol = "https";
+my $port = "1556";
+my $nbmaster;
+my $username;
+my $password;
+my $domainName;
+my $domainType;
+my $token;
+my $base_url;
+my $client;
+
+my $ua = LWP::UserAgent->new(
+                ssl_opts => { verify_hostname => 0, verify_peer => 0},
+            );
+
+# subroutines for printing usage and library information required to run the script.
+sub print_usage {
+    print("\n\nUsage:");
+    print("\nperl get_services -nbmaster <masterServer> -username <username> -password <password> [-domainName <domainName>] [-domainType <domainType>] -client <client>\n\n\n");
+}
+
+sub print_disclaimer {
+    print("--------------------------------------------------------\n");
+    print("--       This script requires Perl 5.20.2 or later    --\n");
+    print("--------------------------------------------------------\n");
+    print("Executing this library requires some additional libraries like \n\t'LWP' \n\t'JSON'\ \n\t'Getopt'\ \n\n");
+    print("You can specify the 'nbmaster', 'username', 'password', 'domainName', 'domainType' and 'client' as command-line parameters\n");
+    print_usage();
+}
+
+
+# subroutine to process user input
+sub user_input {
+    GetOptions(
+        'nbmaster=s' => \$nbmaster,
+        'username=s' => \$username,
+        'password=s' => \$password,
+        'domainName=s' => \$domainName,
+        'domainType=s' => \$domainType,
+        'client=s' => \$client,
+    ) or die print_usage();
+
+    if ($nbmaster eq "") {
+        print("Please provide the value for 'nbmaster'");
+        exit;
+    }
+
+    if ($username eq "") {
+        print("Please provide the value for 'username'");
+        exit;
+    }
+
+    if ($password eq "") {
+        print("Please provide the value for 'password'");
+        exit;
+    }
+
+    if ($client eq "") {
+        print("Please provide the value for 'client'");
+        exit;
+    }
+
+    $base_url = "$protocol://$nbmaster:$port/netbackup";
+}
+
+# subroutine to get hostid for a given host name
+sub get_hostid {
+	my $uuid;
+    my @argument_list = @_;
+    $host_name = $argument_list[0];
+
+    my $url = "$base_url/config/hosts?filter=hostName eq '$host_name'";
+    my $req = HTTP::Request->new(GET => $url);
+    $req->header('Accept' => $content_type_v3);
+    $req->header('Authorization' => $token);
+    print "\n\n**************************************************************";
+    print "\n\n Making GET Request to get host id \n\n";
+
+    my $resp = $ua->request($req);
+    if ($resp->is_success) {
+        my $message = $resp->decoded_content;
+        print "Get host succeeded with status code: ", $resp->code, "\n";
+        my $json = decode_json($message);
+        my @hosts = @{$json->{'hosts'}};
+        $uuid = @hosts[0]->{'uuid'};
+       print "uuid=$uuid\n";
+    }
+    else {
+        print "HTTP GET error code: ", $resp->code, "\n";
+        print "HTTP GET error message: ", $resp->decoded_content, "\n";
+    }
+   return $uuid;
+}
+
+# subroutine to get services
+sub get_services {
+	my @argument_list = @_;
+    my $uuid = $argument_list[0];
+    my $url = "$base_url/admin/hosts/$uuid/services";
+
+    my $req = HTTP::Request->new(GET => $url);
+    $req->header('Accept' => $content_type_v3);
+    $req->header('Authorization' => $token);
+
+    print "\n\n**************************************************************";
+    print "\n\n Making GET Request to get services\n\n";
+
+    my $resp = $ua->request($req);
+    if ($resp->is_success) {
+        my $message = $resp->decoded_content;
+        print "Get services succeeded with status code: ", $resp->code, "\n\n\n";
+        print "Services List for NetBackup Client \"$client\"\n\n";
+        print "id     status\n";
+        print "=======.================\n";
+
+        my $json = decode_json($message);
+        my @services = @{$json->{'data'}};
+        foreach (@services) {
+            my $service = $_;
+            my $id = $service->{'id'};
+            my $status = $service->{'attributes'}->{'status'};
+            printf("%7s %-16s\n", $id, $status);
+        }
+    }
+    else {
+        print "HTTP GET error code: ", $resp->code, "\n";
+        print "HTTP GET error message: ", $resp->decoded_content, "\n";
+    }
+
+    $url = "$base_url/admin/hosts/$uuid/services/bpcd";
+
+    $req = HTTP::Request->new(GET => $url);
+    $req->header('Accept' => $content_type_v3);
+    $req->header('Authorization' => $token);
+
+    print "\n\n**************************************************************";
+    print "\n\n Making GET Request to get the service by name\n\n";
+
+    my $resp = $ua->request($req);
+    if ($resp->is_success) {
+        my $message = $resp->decoded_content;
+        print "Get the service succeeded with status code: ", $resp->code, "\n\n\n";
+        print "Services List for NetBackup Client \"$client\"\n\n";
+        print "id     status\n";
+        print "=======.================\n";
+
+        my $json = decode_json($message);
+        $id = $json->{'data'}->{'id'};
+        $status = $json->{'data'}->{'attributes'}->{'status'};
+        printf("%7s %-16s\n", $id, $status);
+    }
+    else {
+        print "HTTP GET error code: ", $resp->code, "\n";
+        print "HTTP GET error message: ", $resp->decoded_content, "\n";
+    }
+}
+
+print_disclaimer();
+
+user_input();
+
+$token = perform_login($base_url, $username, $password, $domainName, $domainType);
+
+my $uuid = get_hostid($client);
+if ($uuid eq "") {
+	print("\nUnable to read host uuid for client $client\n");
+	exit;
+}
+
+get_services($uuid);
