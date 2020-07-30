cc5468de7db7e016ff7f88e169643f83a5de3c0f
santhosh-vrts <>
2020-07-30 18:02:20 -0500

    Get VMware assets API py script changes

--- recipes/python/assets/get_vmware_assets.py
+++ recipes/python/assets/get_vmware_assets.py
@@ -1,70 +1,66 @@
-import sys
-import login.login_api as login_api
-import requests
+## The script can be run with Python 3.5 or higher version. 
+## The script requires 'requests' library to make the API calls. The library can be installed using the command: pip install requests.
+## No cert setup/verification is done for calling the APIs. Note that the script is only a sample/reference and not for use in any environment.
 
-def print_disclaimer():
-    print("\n-------------------------------------------------------------------------------------------------")
-    print("--                          This script requires Python3.5 or higher.                          --")
-    print("--    The system where this script is run should have Python 3.5 or higher version installed.  --")
-    print("-------------------------------------------------------------------------------------------------")
-    print("The script requires 'requests' library to make the API calls.")
-    print("You can install the library using the command: pip install requests")
-    print("-------------------------------------------------------------------------------------------------")
+import sys
+import argparse
+import requests
+import login.login_api as login_api
 
 def print_usage():
-    print("\nCommand-line usage (should be run from the parent directory of the 'assets' directory):")
-    print("\tpython -Wignore -m assets.get_vmware_assets -nbserver <server> -username <username> -password <password> [-domainName <domainName>] [-domainType <domainType>] [-assetsFilter <filter>]")
-    print("-------------------------------------------------------------------------------------------------")
+    return ("\nThe command should be run from the parent directory of the 'assets' directory:\n"
+            "\tpython -Wignore -m assets.get_vmware_assets -nbserver <server> -username <username> -password <password> "
+            "-domainName <domainName> -domainType <domainType> [-assetsFilter <filter criteria>]\n")
 
+parser = argparse.ArgumentParser(usage = print_usage())
+parser.add_argument('-nbserver', required=True)
+parser.add_argument('-username', required=True)
+parser.add_argument('-password', required=True)
+parser.add_argument('-domainName', required=True)
+parser.add_argument('-domainType', required=True)
+parser.add_argument('-assetsFilter', default="")
+args = parser.parse_args()
 
-print_disclaimer()
-print_usage()
-
-if len(sys.argv)%2 == 0:
-    print("\nInvalid command!")
-    print_usage()
-    exit()
-
-nbserver = ""
-username = ""
-password = ""
-domainName = ""
-domainType = ""
-assetsFilter = ""
-
-for i in range(1, len(sys.argv), 2):
-    if sys.argv[i] == "-nbserver":
-            nbserver = sys.argv[i + 1]
-    elif sys.argv[i] == "-username":
-            username = sys.argv[i + 1]
-    elif sys.argv[i] == "-password":
-            password = sys.argv[i + 1]
-    elif sys.argv[i] == "-domainName":
-            domainName = sys.argv[i + 1]
-    elif sys.argv[i] == "-domainType":
-            domainType = sys.argv[i + 1]
-    elif sys.argv[i] == "-assetsFilter":
-            assetsFilter = sys.argv[i + 1]
-    else:
-            print("\nInvalid command!")
-            print_usage()
-            exit()
-                
-if nbserver == "":
-        print("Please provide the value for 'nbserver'\n")
-        exit()
-elif username == "":
-        print("Please provide the value for 'username'\n")
-        exit()
-elif password == "":
-        print("Please provide the value for 'password'\n")
-        exit()
-elif domainName == "":
-        print("Please provide the value for 'domainName'\n")
-        exit()
-elif domainType == "":
-        print("Please provide the value for 'domainType'\n")
-        exit()
+nbserver = args.nbserver
+username = args.username
+password = args.password
+domainName = args.domainName
+domainType = args.domainType
+assetsFilter = args.assetsFilter
+##
+##for i in range(1, len(sys.argv), 2):
+##    if sys.argv[i] == "-nbserver":
+##            nbserver = sys.argv[i + 1]
+##    elif sys.argv[i] == "-username":
+##            username = sys.argv[i + 1]
+##    elif sys.argv[i] == "-password":
+##            password = sys.argv[i + 1]
+##    elif sys.argv[i] == "-domainName":
+##            domainName = sys.argv[i + 1]
+##    elif sys.argv[i] == "-domainType":
+##            domainType = sys.argv[i + 1]
+##    elif sys.argv[i] == "-assetsFilter":
+##            assetsFilter = sys.argv[i + 1]
+##    else:
+##            print("\nInvalid command!")
+##            print_usage()
+##            exit()
+##
+##if nbserver == "":
+##        print("Please provide the value for 'nbserver'\n")
+##        exit()
+##elif username == "":
+##        print("Please provide the value for 'username'\n")
+##        exit()
+##elif password == "":
+##        print("Please provide the value for 'password'\n")
+##        exit()
+##elif domainName == "":
+##        print("Please provide the value for 'domainName'\n")
+##        exit()
+##elif domainType == "":
+##        print("Please provide the value for 'domainType'\n")
+##        exit()
 
 base_url = "https://" + nbserver + "/netbackup"
 vm_assets_url = base_url + "/asset-service/workloads/vmware/assets"
@@ -98,13 +94,16 @@ def get_vmware_assets():
         if assets_response.status_code != 200:
             print("VMware Assets API failed with status code {} and {}\n".format(resp.status_code, resp.json()))
             raise SystemExit("\n\n")
-        
+
         print_assets(assets['data'])
 
         offset += assets['meta']['pagination']['limit']
         next = assets['meta']['pagination']['hasNext']
 
-def print_assets(assets_data):   
+        if len(assets['data']) == 0:
+            print("No assets returned.")
+
+def print_assets(assets_data):
     for asset in assets_data:
         asset_attrs = asset['attributes']
         asset_common_attrs = asset_attrs['commonAssetAttributes']
@@ -113,7 +112,7 @@ def print_assets(assets_data):
             asset_protection_list = asset_common_attrs['activeProtection']['protectionDetailsList']
             for asset_protection in asset_protection_list:
                 asset_protection_plans.append(asset_protection['protectionPlanName'])
-            
+
         print(asset_common_attrs['displayName'], asset_attrs['instanceUuid'], asset_attrs['vCenter'], asset_protection_plans, sep="\t")
 
 
