51ea86e52aec1c40d6b08e38d4d42a78b0261a06
Santhosh <39775662+santhosh-vrts@users.noreply.github.com>
2020-07-30 19:04:19 -0500

    Asset service VMware assets API sample script
    Asset service - GET VMware assets API sample python script

---
+++ recipes/python/assets/README.md
@@ -0,0 +1,28 @@
+### NetBackup Assets Service API Code Samples
+
+This directory contains Python scripts demonstrating the use of NetBackup Asset Service APIs.
+
+#### Disclaimer
+
+The samples are provided only for reference and not meant for production use.
+
+#### Executing the script
+
+Prerequisites:
+- NetBackup 8.3 or higher
+- Python 3.5 or higher (the script has been tested with Python 3.8)
+- Python modules: `requests`.
+
+
+The following are the commands to run the scripts (should be run from the parent directory of this 'assets' directory).
+
+- get_vmware_assets:
+    `python -Wignore -m assets.get_vmware_assets -nbserver <server> -username <username> -password <password> -domainName <domainName> -domainType <domainType> [-assetsFilter <filter>]`
+
+The script uses the NetBackup Asset Service API to get the VMware workload assets (filtered by the given filter if specified). It prints the details (delimited by tab) such as asset display name, instance Id, vCenter and the plan names that the asset is protected by.
+
+Note: _The assetsFilter option can be used to filter the assets returned. It should be in OData format (refer to the NetBackup API documentation). It is optional. If not specified the script will print all the VM assets. Redirect the script output to a file to avoid printing the details on the terminal._
+
+Examples: `python -Wignore -m assets.get_vmware_assets -nbserver localhost -username user -password password -domainName domain -domainType NT > vm_assets.txt`
+
+`python -Wignore -m assets.get_vmware_assets -nbserver localhost -username user -password password -domainName domain -domainType NT -assetsFilter "contains(commonAssetAttributes/displayName, 'backup')" > vm_assets.txt`
---
+++ recipes/python/assets/get_vmware_assets.py
@@ -0,0 +1,85 @@
+## The script can be run with Python 3.5 or higher version. 
+## The script requires 'requests' library to make the API calls. The library can be installed using the command: pip install requests.
+
+import sys
+import argparse
+import requests
+import login.login_api as login_api
+
+def print_usage():
+    return ("\nThe command should be run from the parent directory of the 'assets' directory:\n"
+            "\tpython -Wignore -m assets.get_vmware_assets -nbserver <server> -username <username> -password <password> "
+            "-domainName <domainName> -domainType <domainType> [-assetsFilter <filter criteria>]\n")
+
+parser = argparse.ArgumentParser(usage = print_usage())
+parser.add_argument('-nbserver', required=True)
+parser.add_argument('-username', required=True)
+parser.add_argument('-password', required=True)
+parser.add_argument('-domainName', required=True)
+parser.add_argument('-domainType', required=True)
+parser.add_argument('-assetsFilter', default="")
+args = parser.parse_args()
+
+nbserver = args.nbserver
+username = args.username
+password = args.password
+domainName = args.domainName
+domainType = args.domainType
+assetsFilter = args.assetsFilter
+
+base_url = "https://" + nbserver + "/netbackup"
+vm_assets_url = base_url + "/asset-service/workloads/vmware/assets"
+
+default_sort = "commonAssetAttributes.displayName"
+
+assetTypeFilter = "(assetType eq 'vm')";
+if assetsFilter != "":
+    assetsFilter = assetsFilter + " and " + assetTypeFilter
+else:
+    assetsFilter = assetTypeFilter
+
+print("\nExecuting the script...")
+
+jwt = login_api.perform_login(base_url, username, password, domainName, domainType)
+
+headers = {'Authorization': jwt}
+
+print("\nGetting VMware assets...")
+print("Printing the following asset details: DisplayName, InstanceId, vCenter, ProtectedByPlanNames\n")
+
+def get_vmware_assets():
+    offset = 0
+    next = True
+    while next:
+        queryparams = {'page[offset]':offset, 'filter':assetsFilter, 'sort':default_sort}
+        assets_response = requests.get(vm_assets_url, headers=headers, params=queryparams, verify=False)
+        assets = assets_response.json()
+
+        if assets_response.status_code != 200:
+            print("VMware Assets API failed with status code {} and {}\n".format(resp.status_code, resp.json()))
+            raise SystemExit("\n\n")
+
+        print_assets(assets['data'])
+
+        offset += assets['meta']['pagination']['limit']
+        next = assets['meta']['pagination']['hasNext']
+
+        if len(assets['data']) == 0:
+            print("No assets returned.")
+
+def print_assets(assets_data):
+    for asset in assets_data:
+        asset_attrs = asset['attributes']
+        asset_common_attrs = asset_attrs['commonAssetAttributes']
+        asset_protection_plans = []
+        if "activeProtection" in asset_common_attrs :
+            asset_protection_list = asset_common_attrs['activeProtection']['protectionDetailsList']
+            for asset_protection in asset_protection_list:
+                asset_protection_plans.append(asset_protection['protectionPlanName'])
+
+        print(asset_common_attrs['displayName'], asset_attrs['instanceUuid'], asset_attrs['vCenter'], asset_protection_plans, sep="\t")
+
+
+get_vmware_assets()
+
+print("\nScript completed successfully!\n")
